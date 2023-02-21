f4c15fb0be09e7219526a649b90057d4b6040db2
Michal Jirku <box@wejn.org>
2023-02-21 17:45:33 +0100

    Fix whitespace

--- clockdivider.v
+++ clockdivider.v
@@ -1,20 +1,20 @@
 module clock_divider(clk, sys_rst_n, slow_clk);
-	input wire clk;
-	input wire sys_rst_n;
-	output reg slow_clk;
+    input wire clk;
+    input wire sys_rst_n;
+    output reg slow_clk;
 
-	parameter COUNTER_SIZE = 21;
-	parameter INCREMENT = 1'd1;
+    parameter COUNTER_SIZE = 21;
+    parameter INCREMENT = 1'd1;
 
-	reg [COUNTER_SIZE-1:0] count;
-	reg pulse;
+    reg [COUNTER_SIZE-1:0] count;
+    reg pulse;
 
-	always @ (posedge clk or negedge sys_rst_n)
-	begin
-		if (!sys_rst_n)
-			{slow_clk, count} <= 0;
-		else
-			{slow_clk, count} <= count + INCREMENT;
-	end
+    always @ (posedge clk or negedge sys_rst_n)
+    begin
+        if (!sys_rst_n)
+            {slow_clk, count} <= 0;
+        else
+            {slow_clk, count} <= count + INCREMENT;
+    end
 endmodule
 
